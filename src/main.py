9cbb977f82a80973af9cac3bfdac600d44e6f240
karambaq <0maximq@gmail.com>
2018-05-05 00:29:05 +0300

    damn.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import os
+import time
 from collections import deque
 from termcolor import colored
 
@@ -8,10 +9,15 @@ if __name__=='__main__':
     sended = deque(maxlen=20)
     while True:
         target_games = get_target_games()
-        for g in target_games:
-            if g.get_id() not in sended:
-                print(colored('Sending %s' % g, 'green'))
-                os.system('python3 send_to_channel.py "%s"' % g)
-                sended.append(g.get_id())
+        if target_games:
+            for g in target_games:
+                if g.get_id() not in sended:
+                    print(colored('Sending %s' % g, 'green'))
+                    os.system('python3 send_to_channel.py "%s"' % g)
+                    sended.append(g.get_id())
+        else:
+            print('No games...')
+            time.sleep(30)
+        
             
 
